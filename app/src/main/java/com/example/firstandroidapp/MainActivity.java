ec17d7d1b69df7de9b6ceb0789ff723d50497a2d
Aleksey Serikov <Alekssserikov@gmail.com>
2021-02-28 23:52:21 +0300

    cc

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.firstandroidapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView plList;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        plList.setText("");
+
+        String [] names = {"Java", "Python", "Ruby", "JavaScript", "Mathlab"};
+
+        Arrays.stream(names).forEach(s -> plList.append(s + "\n"));
+
     }
 }
