f8e6390af3f6b01092a93f9d1b531036d2b54485
Aleksey Serikov <alelekssserikov@gmail.com>
2021-03-02 19:03:06 +0300

    qwe

--- app/src/main/java/com/example/vksearch/MainActivity.java
+++ app/src/main/java/com/example/vksearch/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vksearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 
 import com.example.vksearch.utils.NetworkUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URL;
 
@@ -17,6 +22,40 @@ public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+
+    //param, progress, result
+    class VKQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) { //обрабатываем запрос
+            String response = null;
+
+            try {
+                response = NetworkUtils.getResponseFromUrl(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) { //после обработки запроса в фоне
+            String name = null;
+            String lastName = null;
+
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                JSONObject userInfo = jsonArray.getJSONObject(0);
+                name = userInfo.getString("first_name");
+                lastName = userInfo.getString("last_name");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            result.setText("Name: " + name + "\n" + "Last name: " + lastName); //добавляем рез в наше view
+        }
+    }
 
 
     @Override
@@ -31,15 +70,7 @@ public class MainActivity extends AppCompatActivity {
         //create listener
         View.OnClickListener onClickListener = v -> {
             URL generatedUrl = NetworkUtils.generateUrl(searchField.getText().toString());
-            String response = null;
-
-            try {
-                response = NetworkUtils.getResponseFromUrl(generatedUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            result.setText(response);
+            new VKQueryTask().execute(generatedUrl);
         };
 
         //set listener to button
--- app/src/main/java/com/example/vksearch/utils/NetworkUtils.java
+++ app/src/main/java/com/example/vksearch/utils/NetworkUtils.java
@@ -2,8 +2,10 @@ package com.example.vksearch.utils;
 
 import android.net.Uri;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,12 +16,16 @@ public class NetworkUtils {
     private static final String VK_USERS_GET = "/method/users.get";
     private static final String PARAM_USER_ID = "user_ids";
     private static final String PARAM_VERSION = "v";
+    private static final String PARAM_ACCESS_TOKEN = "access_token";
+    private static final String ACCESS_TOKEN_VALUE
+            = "9931d1ca9931d1ca9931d1ca0b99477f99999319931d1caf90c3a6e1339b0e00ae4b545";
 
     public static URL generateUrl(String userId) {
         Uri buildUri = Uri.parse(VK_API_BASE_URL + VK_USERS_GET)
                 .buildUpon()
                 .appendQueryParameter(PARAM_USER_ID, userId)
-                .appendQueryParameter(PARAM_VERSION, "5.8")
+                .appendQueryParameter(PARAM_VERSION, "5.89")
+                .appendQueryParameter(PARAM_ACCESS_TOKEN, ACCESS_TOKEN_VALUE)
                 .build(); //Uri - универсальный идентификатор ресурса
 
         URL url = null;
@@ -36,8 +42,11 @@ public class NetworkUtils {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try {
             InputStream in = urlConnection.getInputStream(); //получаем http ответ
+
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
             Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A"); //формируем целу строку
+            scanner.useDelimiter("\\A"); //формируем целую строку
 
             boolean hasInput = scanner.hasNext();
             if (hasInput) {
